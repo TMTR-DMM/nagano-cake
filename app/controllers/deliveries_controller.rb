5ab568690a1f9ee7aabee7fc134d04c9fe2d7334
takaya-623 <takaya.suzuki.0623@gmail.com>
2020-12-29 09:32:04 +0000

    first commit

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,7 +1,27 @@
 class DeliveriesController < ApplicationController
+  
   def index
+    @delivery = Delivery.new
+    @deliveries = Dlivery.all
+  end
+
+  def creat
+    @delivery = Delivery.new(delivery_params)
   end
 
   def edit
   end
+  
+  def destroy
+  end
+
+  def update
+  end
+  
+  private
+  
+  def delivery_params
+    params.require(:delivery).permit(:postcode, :adress, :name)
+  end
+  
 end
