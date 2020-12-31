83d30aa8897b6cea313ac564018f3316960afed0
takuya805 <takuya0903exile@gmail.com>
2020-12-31 04:52:23 +0000

    管理者　会員ルート分け

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
       
   def after_sign_in_path_for(resource)
-    customers_path(current_customer) # ログイン後
+   if customer_signed_in? # ログイン後
+      customer_customers_path(current_customer)
+   else
+      admin_homes_top_path
+   end 
   end
 
   def after_sign_out_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+ devise_for :admins, skip: :all
+  devise_scope :admin do
+    get 'admins/sign_in' => 'admins/sessions#new'
+    post 'admins/sign_in' => 'admins/sessions#create'
+    delete 'admins/sign_out' => 'admins/sessions#destroy'   
+  end
   namespace :admin do
     get 'categories/index'
     get 'categories/edit'
@@ -21,7 +27,15 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'homes/top'
   end
-devise_for :customers
+ devise_for :customers, skip: :all
+  devise_scope :customer do
+    get 'customers/sign_in' => 'customers/sessions#new',as: 'new_customer_session'
+    post 'customers/sign_in' => 'customers/sessions#create'
+    delete 'customers/sign_out' => 'customers/sessions#destroy'
+    get 'customers/sign_up' => 'customers/registrations#new',as: 'new_customer_registration'
+    post 'customers' => 'customers/registrations#create'
+    get 'customers/password/new' => 'customers/passwords#new'
+  end
   namespace :customer do
     resource :customers, only: [:show, :edit, :update]
     get 'customers/exit_check' => 'customers#exit_check'
