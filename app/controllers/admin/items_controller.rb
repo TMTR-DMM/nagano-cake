3419e1951ef6bbbbe487a1ca703854fe44afbe93
takuya805 <takuya0903exile@gmail.com>
2021-01-02 06:26:44 +0000

    items追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,26 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
   end
 
   def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_index_path
   end
 
   def edit
   end
+  
+  private
+    def item_params
+      params.require(:item).permit(:name, :caption, :excluding_price, :image_id, :is_deleted)
+    end
 end
