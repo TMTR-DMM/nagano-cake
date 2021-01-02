aabc6ff7e6b1a2f5555c8cc695f33d3f80402e36
wa-r <nyuuuuun14@gmail.com>
2021-01-02 16:31:33 +0000

    管理者側の顧客情報編集ページ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,4 +10,18 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :phone_number, :postcode, :address, :email)
+  end
 end
