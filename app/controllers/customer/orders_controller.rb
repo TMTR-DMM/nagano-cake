7f132e5eaf184860d6e46a33ef3d706656066561
takaya-623 <takaya.suzuki.0623@gmail.com>
2021-01-04 11:00:18 +0000

    first commit

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,16 +1,28 @@
 class Customer::OrdersController < ApplicationController
+  before_action :authenticate_customer!
+  
   def new
+    @order = Order.new
+    @deliveries = current_customer.deliveries
   end
-
+  
   def check
   end
-
-  def thank
+  
+  def create
+    @order = Order.new(order_params)
   end
-
+  
   def index
   end
 
   def show
+  end
+  
+  
+  private
+  
+  def order_params
+    params.requier(:order).permit(:postage, :amount_charged, :payment_method, :postcode, :address, :order_status, :name)
   end
 end
