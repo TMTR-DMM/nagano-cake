1728f9ac1e1bcfed462fd4c2e13763aa5af53781
takaya-623 <takaya.suzuki.0623@gmail.com>
2021-01-04 07:12:26 +0000

    商品一覧ページ修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f89faf9bae0380e40101bd159c2e3914fba07346458dd0789d3951d50298aa55b401483eeeb97a3ef3c5b94bd44ea1d17708df68dba19eb0611e1deb489e190b'
