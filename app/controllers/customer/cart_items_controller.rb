68c309e52a3c8f4379652aa0a82157c5006e398b
misahiro33@gmail.com <ec2-user@ip-172-31-56-203.ec2.internal>
2021-01-04 10:43:44 +0000

    カート内商品途中経過

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,4 +1,20 @@
 class Customer::CartItemsController < ApplicationController
   def index
+    @cart_items = Item.all
+    @cart_total = Item.all.sum(:excluding_price)
+  end
+  
+  def update
+    @cart_items.update(quantity: params[:quantity].to_i)
+    redirect_to current_cart_item
+  end
+  
+  def destroy
+    @cart_items.destroy
+    flash.now[:alert] = "#{@cart_item.product.name}を削除しました"
+    redirect_to current_cart_item
+  end
+  
+  def all_destroy
   end
 end
