4aebc0a5f1db0e0639c30c37acfb091e7395f0b0
takuya805 <takuya0903exile@gmail.com>
2021-01-08 02:29:42 +0000

    日本語表記追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   
   enum payment_method:{"クレジットカード": 0, "銀行振込": 1}
-  enum order_status: {"入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4}
-  #enum order_status: {before_payment: 0, payment_check: 1, making: 2, before_delivery: 3, deliveryed: 4}
+  #enum order_status: {"入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4}
+  enum order_status: {before_payment: 0, payment_check: 1, making: 2, before_delivery: 3, deliveryed: 4}
   
 end
