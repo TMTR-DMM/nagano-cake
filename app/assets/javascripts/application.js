cf9ecb4451418f85a275913c8fd2be514742abea
wa-r <nyuuuuun14@gmail.com>
2021-01-10 12:48:41 +0000

    スライドショー完成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
     // スライドショーの変化（"fade" or "slide"）
     transition : 'slide',
     // 変化にかかる時間（ミリ秒）
-    speed : 1000,
+    speed : 3000,
     // easingの種類
     easing : 'easeOutQuart',
     // ナビゲーションの形（"block" or "bubble"）
@@ -37,11 +37,11 @@ $(document).ready(function () {
     // スライドショーの自動再生（falseで自動再生なし）
     autoPlay : true,
     // 自動再生時のスライド切替間隔（ミリ秒）
-    autoPlayDuration : 4000,
+    autoPlayDuration : 3000,
     // キーボードの矢印キーによるスライド送りの設定（trueで有効）
     keyboardOnAlways : false,
     // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
-    hidePrevious : true
+    hidePrevious : true,
   });
 });
 
