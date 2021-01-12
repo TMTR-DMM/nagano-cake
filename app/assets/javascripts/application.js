f9cfd3a12ccc26712d5ab700307ca61a11a94409
EC2 Default User <ec2-user@ip-172-31-15-81.ap-northeast-1.compute.internal>
2021-01-12 08:06:37 +0000

    更新

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@
 
 /* global $*/
 // オプションを指定してSkipprの実行
-$(document).ready("turbolinks:load",function () {
+$(document).on("turbolinks:load",function () {
   $(".theTarget").skippr({
     // スライドショーの変化（"fade" or "slide"）
     transition : 'slide',
